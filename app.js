c40e96f061253db03125e6c7d25bcca23be035f3
mileRANT <liem.htran@outlook.com>
2023-06-16 14:40:37 -0400

    Register and Login route [post] working. Level 1 security.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,10 +12,24 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//connect to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
+const userSchema = {
+    email: String,
+    password: String
+};
 
+const User = new mongoose.model("User", userSchema);
 
+async function addUser(newUser){
+    const addedUser = await newUser.save();
+}
 
+async function loginUser(username){
+    const findUser = await User.findOne({email: username});
+    return findUser;
+}
 
 app.get("/", function(req,res){
     res.render("home");
@@ -24,11 +39,69 @@ app.get("/login", function(req,res){
     res.render("login");
 });
 
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{  
+        loginUser(username).then(function(foundUser){
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets");
+                }else {
+                    console.log("incorrect password")
+                };
+            };
+        });
+    } catch(err) {
+        console.log(err)
+    };
+    // User.findOne({email: username}, function(err, foundUser){
+    //     if(err) {
+    //         console.log(err)
+    //     } else {
+    //         if (foundUser) {
+    //             if (foundUser.password === password){
+    //                 res.render("secrets");
+    //             }
+    //         }
+    //     }
+    // });
+});
+
 app.get("/register", function(req,res){
     res.render("register");
 });
 
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    console.log(newUser);
+
+//we need to be able to catch this error and not show our secrets
+    try{
+        addUser(newUser).then(function(){
+            res.render("secrets");
+        });
+    } catch(err) {
+        console.log(err);
+    };
+
+
+    //angela yu's version below which no longer works as callback is no longer supported by mongoose
+    // newUser.save(function(err){
+    //     if (err){
+    //         console.log(err);
+    //     } else {
+    //         res.render("secrets");
+    //     }
+    // });
+    // res.render("secrets");
+});
+
 
 app.listen(3000, function(){
-    console.log*"Server started on port 3000"
+    console.log("Server started on port 3000")
 });
